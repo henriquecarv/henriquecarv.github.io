d928443b7c4cb94b00b641d1382fce7afca2026e
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2017-06-20 22:34:28 -0300

    Codacy error fixes and NPM packages updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,58 @@
 // Include gulp
-var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
-var concatCss = require('gulp-concat-css');
-var browserSync = require('browser-sync').create();
-var config = require('./sourcePath');
-var jshint = require('gulp-jshint');
-var minify = require('gulp-minify');
+var gulp = require("gulp");
+var cleanCSS = require("gulp-clean-css");
+var concatCss = require("gulp-concat-css");
+var browserSync = require("browser-sync").create();
+var config = require("./sourcePath");
+var jshint = require("gulp-jshint");
+var minify = require("gulp-minify");
 
-gulp.task('css', function () {
-    gulp.src('css/*.css')
-        .pipe(concatCss('app.css'))
+gulp.task("css", function () {
+    gulp.src("css/*.css")
+        .pipe(concatCss("app.css"))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('publish/css'))
+        .pipe(gulp.dest("publish/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('js', function () {
-    gulp.src('js/*.js')
+gulp.task("js", function () {
+    gulp.src("js/*.js")
         .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(jshint.reporter('fail'))
+        .pipe(jshint.reporter("default"))
+        .pipe(jshint.reporter("fail"))
         .pipe(minify())
-        .pipe(gulp.dest('publish/js'))
+        .pipe(gulp.dest("publish/js"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('publishJsVendors', function () {
+gulp.task("publishJsVendors", function () {
     gulp.src(config.jsVendorsSourcePath)
         .pipe(gulp.dest(config.jsVendorsDestPath));
 });
 
-gulp.task('publishCssVendors', function () {
+gulp.task("publishCssVendors", function () {
     gulp.src(config.cssVendorsSourcePath)
         .pipe(gulp.dest(config.cssVendorsDestPath));
 });
 
-gulp.task('publishFontRobotoVendors', function () {
+gulp.task("publishFontRobotoVendors", function () {
     gulp.src(config.fontVendorsRobotoSourcePath)
         .pipe(gulp.dest(config.fontVendorsRobotoDestPath));
 });
 
-gulp.task('publishFontAwesomeVendors', function () {
+gulp.task("publishFontAwesomeVendors", function () {
     gulp.src(config.fontVendorsFontAwesomeSourcePath)
         .pipe(gulp.dest(config.fontVendorsFontAwesomeDestPath));
 });
 
-gulp.task('serve', function () {
+gulp.task("serve", function () {
     browserSync.init({
         server: "./"
     });
 })
 
-gulp.task('default', ['serve', 'css', 'js', 'publishJsVendors', 'publishCssVendors', 'publishFontRobotoVendors', 'publishFontAwesomeVendors'], function () {
-    gulp.watch('css/*.css', ['css']);
-    gulp.watch('js/*.js', ['js']);
-    gulp.watch('./*.html').on('change', browserSync.reload);
+gulp.task("default", ["serve", "css", "js", "publishJsVendors", "publishCssVendors", "publishFontRobotoVendors", "publishFontAwesomeVendors"], function () {
+    gulp.watch("css/*.css", ["css"]);
+    gulp.watch("js/*.js", ["js"]);
+    gulp.watch("./*.html").on("change", browserSync.reload);
 });
