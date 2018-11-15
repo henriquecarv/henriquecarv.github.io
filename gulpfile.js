f89297621eb4812c5725475df5d00e1d706a0978
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2018-11-15 19:07:23 -0200

    Add babel

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel');
 const concatCss = require('gulp-concat-css');
 const browserSync = require('browser-sync').create();
 const config = require('./sourcePath');
@@ -10,128 +11,111 @@ const htmlmin = require('gulp-htmlmin');
 const uglify = require('gulp-uglify');
 
 const paths = {
-	js: 'js/*.js',
-	css: 'css/*.css',
-	html: 'html/index.html',
+  js: 'js/*.js',
+  css: 'css/*.css',
+  html: 'html/index.html',
 };
 
 let isDev = false;
 
 const toggleIsDev = done => {
-	isDev = !isDev;
-	done();
+  isDev = !isDev;
+  done();
 };
 
 const css = () => {
-	let result = gulp.src(paths.css).pipe(concatCss('app.css'));
+  let result = gulp.src(paths.css).pipe(concatCss('app.css'));
 
-	if (!isDev) {
-		result = result.pipe(cleanCSS({compatibility: 'ie8'}));
-	}
+  if (!isDev) {
+    result = result.pipe(cleanCSS({compatibility: 'ie8'}));
+  }
 
-	result = result.pipe(gulp.dest('publish/css')).pipe(browserSync.stream());
+  result = result.pipe(gulp.dest('publish/css')).pipe(browserSync.stream());
 
-	return result;
+  return result;
 };
 
 const js = () => {
-	let result = gulp.src(paths.js);
+  let result = gulp.src(paths.js).pipe(
+    babel({
+      presets: ['@babel/env'],
+    })
+  );
 
-	if (!isDev) {
-		result = result
-			.pipe(uglify())
-			.pipe(gulp.dest('publish/js'))
-			.pipe(browserSync.stream());
-	} else {
-		result = result
-			.pipe(sourcemaps.init())
-			.pipe(sourcemaps.write())
-			.pipe(gulp.dest('publish/js'))
-			.pipe(browserSync.stream());
-	}
+  if (!isDev) {
+    result = result
+      .pipe(uglify())
+      .pipe(gulp.dest('publish/js'))
+      .pipe(browserSync.stream());
+  } else {
+    result = result
+      .pipe(sourcemaps.init())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('publish/js'))
+      .pipe(browserSync.stream());
+  }
 
-	return result;
+  return result;
 };
 
 const html = () => {
-	let result = gulp.src(paths.html);
+  let result = gulp.src(paths.html);
 
-	if (!isDev) {
-		result = result.pipe(
-			htmlmin({
-				collapseWhitespace: true,
-				removeComments: true,
-				removeEmptyAttributes: true,
-			})
-		);
-	}
+  if (!isDev) {
+    result = result.pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+      })
+    );
+  }
 
-	result = result.pipe(gulp.dest('./')).pipe(browserSync.stream());
+  result = result.pipe(gulp.dest('./')).pipe(browserSync.stream());
 
-	return result;
+  return result;
 };
 
 const publishJsVendors = () => {
-	return gulp
-		.src(config.jsVendorsSourcePath)
-		.pipe(gulp.dest(config.jsVendorsDestPath));
+  return gulp.src(config.jsVendorsSourcePath).pipe(gulp.dest(config.jsVendorsDestPath));
 };
 
 const publishCssVendors = () => {
-	return gulp
-		.src(config.cssVendorsSourcePath)
-		.pipe(gulp.dest(config.cssVendorsDestPath));
+  return gulp.src(config.cssVendorsSourcePath).pipe(gulp.dest(config.cssVendorsDestPath));
 };
 
 const publishFontRobotoVendors = () => {
-	return gulp
-		.src(config.fontVendorsRobotoSourcePath)
-		.pipe(gulp.dest(config.fontVendorsRobotoDestPath));
+  return gulp.src(config.fontVendorsRobotoSourcePath).pipe(gulp.dest(config.fontVendorsRobotoDestPath));
 };
 
 const publishFontMaterialVendors = () => {
-	return gulp
-		.src(config.fontVendorsMaterialSourcePath)
-		.pipe(gulp.dest(config.fontVendorsMaterialDestPath));
+  return gulp.src(config.fontVendorsMaterialSourcePath).pipe(gulp.dest(config.fontVendorsMaterialDestPath));
 };
 
 const publishFontAwesomeVendors = () => {
-	return gulp
-		.src(config.fontVendorsFontAwesomeSourcePath)
-		.pipe(gulp.dest(config.fontVendorsFontAwesomeDestPath));
+  return gulp.src(config.fontVendorsFontAwesomeSourcePath).pipe(gulp.dest(config.fontVendorsFontAwesomeDestPath));
 };
 
 const reload = done => {
-	browserSync.reload();
-	done();
+  browserSync.reload();
+  done();
 };
 
 const serve = done => {
-	browserSync.init({
-		server: {
-			baseDir: './',
-		},
-	});
-	done();
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+  done();
 };
 
-const build = gulp.series(
-	gulp.parallel(
-		css,
-		js,
-		publishJsVendors,
-		publishCssVendors,
-		publishFontRobotoVendors,
-		publishFontMaterialVendors,
-		publishFontAwesomeVendors
-	),
-	html
-);
+const build = gulp.series(gulp.parallel(css, js, publishJsVendors, publishCssVendors, publishFontRobotoVendors, publishFontMaterialVendors, publishFontAwesomeVendors), html);
 
 const watch = () => {
-	gulp.watch(paths.css, gulp.series(css, reload));
-	gulp.watch(paths.js, gulp.series(js, reload));
-	gulp.watch(paths.html, gulp.series(html, reload));
+  gulp.watch(paths.css, gulp.series(css, reload));
+  gulp.watch(paths.js, gulp.series(js, reload));
+  gulp.watch(paths.html, gulp.series(html, reload));
 };
 
 gulp.task('build', build);
--- js/app.js
+++ js/app.js
@@ -1,47 +1,39 @@
 (function($) {
-	$(document).ready(function() {
-		(function(i, s, o, g, r, a, m) {
-			i['GoogleAnalyticsObject'] = r;
-			(i[r] =
-				i[r] ||
-				function() {
-					(i[r].q = i[r].q || []).push(arguments);
-				}),
-				(i[r].l = 1 * new Date());
-			(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-			a.async = 1;
-			a.src = g;
-			m.parentNode.insertBefore(a, m);
-		})(
-			window,
-			document,
-			'script',
-			'https://www.google-analytics.com/analytics.js',
-			'ga'
-		);
+  $(document).ready(() => {
+    ((i, s, o, g, r, a, m) => {
+      i['GoogleAnalyticsObject'] = r;
+      (i[r] =
+        i[r] ||
+        function() {
+          (i[r].q = i[r].q || []).push(arguments);
+        }),
+        (i[r].l = 1 * new Date());
+      (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m);
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-		ga('create', 'UA-89548159-1', 'auto');
-		ga('send', 'pageview');
+    ga('create', 'UA-89548159-1', 'auto');
+    ga('send', 'pageview');
 
-		var changeWidth = function() {
-			var smallProfilePicture =
-				'https://www.gravatar.com/avatar/45dc36c036a3034db00361897cd500f0.jpg?s=130';
-			var bigProfilePicture =
-				'https://www.gravatar.com/avatar/45dc36c036a3034db00361897cd500f0.jpg?s=170';
-			if ($(window).width() < 480) {
-				$('.btn')
-					.removeClass('btn-lg')
-					.addClass('btn-md');
-				$('.img-fluid').attr('src', smallProfilePicture);
-			} else {
-				$('.btn')
-					.removeClass('btn-md')
-					.addClass('btn-lg');
-				$('.img-fluid').attr('src', bigProfilePicture);
-			}
-		};
+    const changeWidth = () => {
+      const smallProfilePicture = 'https://www.gravatar.com/avatar/45dc36c036a3034db00361897cd500f0.jpg?s=130';
+      const bigProfilePicture = 'https://www.gravatar.com/avatar/45dc36c036a3034db00361897cd500f0.jpg?s=170';
+      if ($(window).width() < 480) {
+        $('.btn')
+          .removeClass('btn-lg')
+          .addClass('btn-md');
+        $('.img-fluid').attr('src', smallProfilePicture);
+      } else {
+        $('.btn')
+          .removeClass('btn-md')
+          .addClass('btn-lg');
+        $('.img-fluid').attr('src', bigProfilePicture);
+      }
+    };
 
-		changeWidth();
-		$(window).resize(changeWidth);
-	});
+    changeWidth();
+    $(window).resize(changeWidth);
+  });
 })(jQuery);
