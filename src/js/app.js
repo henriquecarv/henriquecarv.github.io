5d5b604d5768d106da4d50777a37e64db25dcf9c
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2019-05-30 15:37:29 +0200

    Update packages

--- src/js/app.js
+++ src/js/app.js
@@ -2,19 +2,19 @@
 (($) => {
   $(document).ready(() => {
     const isMobile = $(window).width() < 480;
-    const urlProfilePicture = 'https://www.gravatar.com/avatar/45dc36c036a3034db00361897cd500f0.jpg?s=';
+    const urlProfilePicture = '/img/profile';
 
     const changeWidth = () => {
       if (isMobile) {
         $('.btn')
           .removeClass('btn-lg')
           .addClass('btn-md');
-        $('.img-fluid').attr('src', `${urlProfilePicture}130`);
+        $('.img-fluid').attr('src', `${urlProfilePicture}_130.jpeg`);
       } else {
         $('.btn')
           .removeClass('btn-md')
           .addClass('btn-lg');
-        $('.img-fluid').attr('src', `${urlProfilePicture}170`);
+        $('.img-fluid').attr('src', `${urlProfilePicture}_170.jpeg`);
       }
     };
 
